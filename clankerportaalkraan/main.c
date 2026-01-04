6242d533218321a6e03a45c9ea718598edf62328
Rubgro <243635516+Rubgro@users.noreply.github.com>
2026-01-04 14:05:15 +0100

    Update main.c

--- clankerportaalkraan/main.c
+++ clankerportaalkraan/main.c
@@ -3,17 +3,24 @@
 
 #include <avr/io.h>
 
-int main(void)
+int main(void) // PK7 = Port K met bit 7 en is ervoor de schakelaaar, PK6 = Port K met bit 6 is er voor de Elektromagneet
 {
+    DDRK &= ~(1 << PK7); // input van de arduino op PK7
 
-    // Insert clanker
+    PORTK |= (1 << PK7); // Pull - up weerstand op PK7
 
-    while(1)
-        //test
-        DDRB |= (1<<PB7);
+    DDRK |= (1 << PK6); // output van de arduino op PK6
 
 
-    ;
-
-    return 0;
+    while (1)
+    {
+        if ((PINK & (1 << PK7)) == 0) // controleerd of de schakelaar ingedrukt is
+        {
+            PORTK |= (1 << PK6); // zet de schakelaar aan zodat er over de Elektro Magneet elektrisiteit kan lopen
+        }
+        else
+        {
+             PORTK &= ~(1 << PK6); // zet de uit zodat er geen elektrisiteit meer over de Elektro Magneet kan lopen
+        }
+    }
 }
